5f222b84288e95039b2e96a7024b2c269d67f82a
Antarya <antarya@gmail.com>
2017-04-12 18:16:10 +0400

    dialog for contact and about not finished yet, some admin changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,10 @@
 //
 //= require jquery.min
 //= require jquery_ujs
+//= require leaflet
+//
+//
+//
 //* require bootstrap-sprockets
 
 //* require cocoon
@@ -34,7 +38,6 @@
 // application script for Charisma theme
 //* require charisma
 
-//* require leaflet
 
 //* require leaflet-sidebar.min
 //* require leaflet-function-button
@@ -113,4 +116,136 @@ $(document).ready(function(){
   })
 
   $('.flash').delay(10000).fadeOut(2000, function () { $(this).remove(); })
+
+  /* ------------------------------- dialog -------------------------------*/
+    const dialog = $('dialog')
+    const dialog_callbacks = {
+      contact: function () {
+        console.log("called")
+        contact_map.invalidateSize()
+      }
+    }
+    $('dialog .dialog-close a').on('click', function (event) {
+      dialog_close();
+    })
+    function dialog_close () {
+      dialog.removeAttr('open')
+      dialog.find('.dialog-page[data-showing]').removeAttr('data-showing')
+    }
+    function dialog_open (page) {
+      dialog_close();
+      dialog.find('.dialog-page[data-bind="' + page + '"]').attr('data-showing', true)
+      console.log(page, dialog_callbacks.hasOwnProperty(page))
+      dialog.attr('open', true)
+      if(dialog_callbacks.hasOwnProperty(page)) {
+        dialog_callbacks[page]();
+      }
+    }
+    $('a[data-dialog-link]').on('click', function () {
+      const page = $(this).attr('data-dialog-link')
+      dialog_open(page)
+    })
+
+    /* ------------------------------- dialog -------------------------------*/
+
+
+    contact_map = L.map('contact_map', {
+      zoomControl: false
+    }).setView([41.70978, 44.76133], 17);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '<a href="http://osm.org/copyright">OpenStreetMap</a>'
+    }).addTo(contact_map);
+
+
+    L.marker([41.70995, 44.76134], {icon: pin}).addTo(contact_map)
+    $('#contact_map_zoom_in').click(function(){
+      contact_map.setZoom(contact_map.getZoom() + 1)
+    });
+
+
+    // zoom out function
+    $('#contact_map_zoom_out').click(function(){
+      contact_map.setZoom(contact_map.getZoom() - 1)
+    });
+        // .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        // .openPopup();
+
+
+    // L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
+
+    // var contact_map = L.map('contact_map').setView([51.505, -0.09], 13);
+    // L.tileLayer('https://api.mapbox.com/styles/v1/antarya/cj1eyowie00iu2rqssh9phros/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW50YXJ5YSIsImEiOiJjaXl5MDBhczAwMGYyMnFwbDA0eWEwdXYyIn0.7W7ecKO77P8GTP7f1wHrXQ', {foo: 'bar'})
+    // .addTo(contact_map);
+
+//     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'your.mapbox.project.id',
+//     accessToken: 'your.mapbox.public.access.token'
+// })
 })
+
+let contact_map
+    var pin = L.icon({
+        iconUrl: '/assets/svg/pin.svg',
+        //iconRetinaUrl: 'my-icon@2x.png',
+        iconSize: [28, 36],
+        // iconAnchor: [-14, -18],
+        // popupAnchor: [-3, -76],
+        // shadowUrl: 'my-icon-shadow.png',
+        // shadowRetinaUrl: 'my-icon-shadow@2x.png',
+        // shadowSize: [68, 95],
+        // shadowAnchor: [22, 94]
+    });
+
+
+
+  //  }
+  //     #in{
+  //       position: absolute;
+  //       top: 20px;
+  //       left: 20px;
+  //       padding-right: 10px;
+  //       padding-left: 10px;
+  //       z-index: 9000;
+  //       background-color: #69D2E7;
+  //       cursor:pointer;
+
+  //     }
+  //     #out{
+  //       position: absolute;
+  //       top: 70px;
+  //       left: 20px;
+  //       padding-right: 5px;
+  //       padding-left: 10px;
+  //       z-index: 9000;
+  //       background-color: #A7DBDB;
+  //       cursor:pointer;
+  //     }
+
+  //   </style>
+  // </head>
+  // <body>
+  //   <div id="map"></div>
+  //   <div id = 'in'><p>Click to Zoom in</p></div>
+  //   <div id = 'out'><p>Click to Zoom out</p></div>
+
+
+  //   <script type="text/javascript">
+  //     var layer;
+  //     var vector = [];
+  //     var legend;
+
+  //     function main() {
+
+  //       var map = L.map('map', {
+  //         zoomControl: false,
+  //         center: [41.390205, 2.154007],
+  //         zoom: 12,
+  //         minZoom:9,
+  //         maxZoom:13
+  //       });
+
+  //       // zoom in function
+  //
