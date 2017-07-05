d062d353a8cdb67221e507819a70d2077739c598
Antarya <antarya@gmail.com>
2017-07-05 16:36:17 +0400

    Uncomment migration for unused tables

--- db/migrate/20170513180206_remove_unused_tables.rb
+++ db/migrate/20170513180206_remove_unused_tables.rb
@@ -1,15 +1,26 @@
 class RemoveUnusedTables < ActiveRecord::Migration
   def change
-    #remove_table :simple_captcha_data
-    #remvoe_table :settings
-    #posts
-    # faq
-    # location
-    # #items
-    # district
-    # delayed jobs
-    # #categories
-    # areas
-    # # todos
+    remove_table :areas
+    remove_table :categories
+    remove_table :categories_posts
+    remove_table :category_translations
+    remove_table :delayed_jobs
+    remove_table :district_translations
+    remove_table :faqs
+    remove_table :faq_translations
+    remove_table :items
+    remove_table :item_translations
+    remove_table :location
+    remove_table :location_translations
+    remove_table :map_tiles
+    remove_table :posts
+    remove_table :post_items
+    remove_table :post_locations
+    remove_table :post_translations
+    remove_table :post_users
+    remove_table :settings
+    remove_table :setting_translations
+    remove_table :simple_captcha_data
+    remove_table :todos
   end
 end
