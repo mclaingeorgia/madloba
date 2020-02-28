d4d73c94682f88292d183323d62f1e2f47d1f34e
Jason <jason.addie@gmail.com>
2020-02-28 07:48:52 +0100

    update registration email

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -3,7 +3,7 @@ class CustomDeviseMailer < DeviseMailer
   include ApplicationHelper
 
   def confirmation_instructions(record, token, opts={})
-    @site_name = t('app.common.name')
+    @site_name = t('app.common.name2')
     @user = record
     super
   end
